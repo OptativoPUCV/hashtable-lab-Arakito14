28f60ff6dc37694d0bdb5983bdf186306b48230d
repl.it user <replituser@example.com>
2020-09-28 03:08:13 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -86,9 +86,7 @@ void * searchMap(HashMap * map,  char * key) {
   long pos = hash(key, map -> capacity);
   map->current = pos;
   for(;map->buckets[pos]->key != key;pos++){
-    if(map->buckets[pos]==NULL){
-      return NULL;
-    }
+    
     if(pos >= map -> capacity){
       pos = 0;
     }
